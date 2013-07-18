c7f9dd50cc05e64ba302c43bb67bbb4f2b23b6d2
ktbyron <katherine_byron@brown.edu>
2013-07-18 11:43:56 -0400

    Updated the CO Scraper
    Fixed find_phone,  find_fips, standardized output, manual fix for curly
    quotes around a middle name.

--- colorado.py
+++ colorado.py
@@ -29,17 +29,17 @@ result = [("authority_name", "first_name", "last_name", "county_name", "fips",
 
 
 file_path = cdir + "colorado-clerks.pdf"
-# url = "http://www.sos.state.co.us/pubs/elections/Resources/files/CountyClerkRosterWebsite.pdf"
-# user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-# headers = {'User-Agent' : user_agent}
+url = "http://www.sos.state.co.us/pubs/elections/Resources/files/CountyClerkRosterWebsite.pdf"
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent' : user_agent}
 
-# req = urllib2.Request(url, "", headers)
-# pdf = urllib2.urlopen(req).read()
+req = urllib2.Request(url, "", headers)
+pdf = urllib2.urlopen(req).read()
 
-# data = dogcatcher.pdf_to_text(pdf)
-# output = open(file_path, "w")
-# output.write(data)
-# output.close()
+data = dogcatcher.pdf_to_text(pdf)
+output = open(file_path, "w")
+output.write(data)
+output.close()
 
 data = open(file_path).read()
 
@@ -67,6 +67,7 @@ for i in two_digits:
 
 data = data.replace("  "," ")
 
+
 #When we clean the PDF, the data breaks down into a handful of regular patterns. These are lines that looks like
 #names, lines that look like addresses or PO Boxes, and lines that looks like phones + emails.
 #So we turn all of these into lists, and later put items with the same list index together.
@@ -91,6 +92,7 @@ for po in po_full:
 
 for county in names_block:
 
+
     authority_name, first_name, last_name, county_name, town_name, fips, street, city, address_state, zip_code, po_street, po_city, po_state, po_zip_code, reg_authority_name, reg_first, reg_last, reg_street, reg_city, reg_state, reg_zip_code, reg_po_street, reg_po_city, reg_po_state, reg_po_zip_code, reg_phone, reg_fax, reg_email, reg_website, reg_hours, phone, fax, email, website, hours, review = dogcatcher.begin(voter_state)
 
     index = names_block.index(county)
@@ -100,6 +102,10 @@ for county in names_block:
     county_split = county.partition(" \n")
     county_name = county_split[0].title()
     official_name = county_split[2]
+    
+    #Manual fix for the curly quotes around Madene Cumming of Phillips county
+    if re.search('Beth', official_name) is not None:
+        official_name = "Madene Beth Cumming" 
 
     first_name, last_name, review = dogcatcher.split_name(official_name, review, "ignore")
 
@@ -107,8 +113,8 @@ for county in names_block:
     
     address_state = voter_state
 
-    phone = dogcatcher.phone_find(phone_re, county_phones)
-    fax = dogcatcher.phone_find(fax_re, county_phones)
+    phone = dogcatcher.find_phone(phone_re, county_phones)
+    fax = dogcatcher.find_phone(fax_re, county_phones)
     email = dogcatcher.find_emails(email_re, county_phones)
 
     #First we find the city, which divides the street and the CSZ + P.O. box (if the latter exists.)
@@ -135,7 +141,7 @@ for county in names_block:
         po_city = city
         po_state = address_state
 
-    fips = dogcatcher.fips_find(county_name, voter_state)
+    fips = dogcatcher.find_fips(county_name, voter_state)
 
     result.append([authority_name, first_name, last_name, county_name, fips,
     street, city, address_state, zip_code,
@@ -148,9 +154,4 @@ for county in names_block:
 
 #This outputs the results to a separate text file.
 
-output = open(cdir + "oklahoma.txt", "w")
-for r in result:
-    r = h.unescape(r)
-    output.write("\t".join(r))
-    output.write("\n")
-output.close()
+dogcatcher.output(result, voter_state, cdir)
