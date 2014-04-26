a9117fd298062fcb1db97e94bced7525b5c2e8fb
Erik Ogan <erik@ogan.net>
2014-04-26 16:01:36 -0700

    FIPS data has the " County" removed from names. Normalize the data returning from Google to match

--- dogcatcher.py
+++ dogcatcher.py
@@ -182,6 +182,7 @@ def map_fips(city, state, zip_code, alternate_city = ""):
 	import urllib
 	import json
 	import sys
+	import re
 
 	#To begin with, we define an address for the place and grab it from the Google Maps API. Since this leaves us with a pile of JSON, we load it into a JSON object.
 
@@ -220,6 +221,8 @@ def map_fips(city, state, zip_code, alternate_city = ""):
 		if unicode("administrative_area_level_2") in json_address[i]['types']:
 			county_name = json_place['results'][0]['address_components'][i]['long_name'].encode('ascii')
 			break
+
+	county_name = re.sub(r'\s*County$', '', county_name, 0, re.IGNORECASE)
 	print [county_name]
 	print "++++++++++++++++++"
 
